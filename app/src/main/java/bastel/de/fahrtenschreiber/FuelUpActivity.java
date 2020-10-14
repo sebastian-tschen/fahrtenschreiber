d8d9435e2bf8cb48042b54c5323f3fd997a833e4
Sebastian Behrens <sebastian.tschen.behrens@gmail.com>
2020-10-14 10:40:40 +0200

    start fuel up activity

--- app/src/main/java/bastel/de/fahrtenschreiber/FuelUpActivity.java
+++ app/src/main/java/bastel/de/fahrtenschreiber/FuelUpActivity.java
@@ -1,4 +1,29 @@
 package bastel.de.fahrtenschreiber;
 
+import android.view.View;
+
 public class FuelUpActivity extends FahrtenschreiberActivity {
+    public void backspace(View view) {
+    }
+
+    public void delete(View view) {
+    }
+
+    public void setFull(View view) {
+    }
+
+    public void writeEntry(View view) {
+    }
+
+    public void pickDriver(View view) {
+    }
+
+    public void pickDate(View view) {
+    }
+
+    public void backspace_fuel(View view) {
+    }
+
+    public void delete_fuel(View view) {
+    }
 }
